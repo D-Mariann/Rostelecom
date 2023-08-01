5ed5e0784df819be67a1f7a095ba1b0da3333052
D-Mariann <119792440+D-Mariann@users.noreply.github.com>
2023-08-01 15:46:04 +0300

    Update conftest.py

--- conftest.py
+++ conftest.py
@@ -20,12 +20,55 @@ from pages.auth_page import AuthPage
 #         pickle.dump(browser.get_cookies(), cookies)
 
 
-@pytest.fixture(autouse=True)
-def browser():
+@pytest.fixture
+def driver():
     driver = webdriver.Chrome(executable_path='\driver\chromedriver.exe')
+
+    # Можно задавать нужный вам размер экрана
+    # driver.set_window_size(1080, 800)
+
     driver.maximize_window()
-    yield driver
-    driver.quit()
+    return driver
+
+
+@pytest.fixture
+def web_browser(request, driver): #передаем сюда каждый тесткейс, вызывается до тесткейса
+    #
+
+    browser = driver
+    # browser.set_window_size(1400, 1000)
+
+    # Вернуть объект браузера
+    yield browser #тут переходим внутрь тесткейса
+
+    # Этот код выполнится после отрабатывания теста:
+    if request.node.rep_call.failed:
+        # Сделать скриншот, если тест провалится:
+        try:
+            browser.execute_script("document.body.bgColor = 'white';")
+
+            # Создаем папку screenshots и кладем туда скриншот с генерированным именем:
+            browser.save_screenshot('screenshots/' + str(uuid.uuid4()) + '.png')
+
+            # Для дебагинга, печатаем информацию в консоль
+            print('URL: ', browser.current_url)
+            print('Browser logs:')
+            for log in browser.get_log('browser'):
+                print(log)
+        except:
+            pass # just ignore any errors here
+
+
+@pytest.hookimpl(hookwrapper=True, tryfirst=True)
+def pytest_runtest_makereport(item, call): #при каждом упавшем тесткейсе, делается скрин, пишется в консоль логи браузера, url, скрин
+    # This function helps to detect that some test failed
+    # and pass this information to teardown:
+
+    outcome = yield
+    rep = outcome.get_result()
+    setattr(item, "rep_" + rep.when, rep)
+    return rep #сохр эту переменную в статус тесткейса
+
 
 @pytest.fixture
 def chrome_options(chrome_options):
@@ -35,52 +78,6 @@ def chrome_options(chrome_options):
     chrome_options.add_argument('--log-level=DEBUG')
 
     return chrome_options
-
-
-@pytest.hookimpl(hookwrapper=True, tryfirst=True)
-def pytest_runtest_makereport(item, call):
-    # This function helps to detect that some test failed
-    # and pass this information to teardown:
-
-    outcome = yield
-    rep = outcome.get_result()
-    setattr(item, "rep_" + rep.when, rep)
-    return rep
-
-
-@pytest.fixture
-def web_browser(request, selenium):
-
-    browser = selenium
-    #browser.set_window_size(1400, 1000)
-    browser.maximize_window()
-
-    # Return browser instance to test case:
-    yield browser
-
-    # Do teardown (this code will be executed after each test):
-
-    if request.node.rep_call.failed:
-        # Make the screen-shot if test failed:
-        try:
-            browser.execute_script("document.body.bgColor = 'white';")
-
-            # Make screen-shot for local debug:
-            browser.save_screenshot('screenshots/' + str(uuid.uuid4()) + '.png')
-
-            # Attach screenshot to Allure report:
-            allure.attach(browser.get_screenshot_as_png(),
-                          name=request.function.__name__,
-                          attachment_type=allure.attachment_type.PNG)
-
-            # For happy debugging:
-            print('URL: ', browser.current_url)
-            print('Browser logs:')
-            for log in browser.get_log('browser'):
-                print(log)
-
-        except:
-            pass # just ignore any errors here
 
 
 def get_test_case_docstring(item):
