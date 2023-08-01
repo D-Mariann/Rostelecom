ce5dd10006c07f70abcb987e0da95e2137e70d77
D-Mariann <119792440+D-Mariann@users.noreply.github.com>
2023-08-01 15:44:50 +0300

    Update test_smoke.py

--- tests/test_smoke.py
+++ tests/test_smoke.py
@@ -2,15 +2,26 @@ from pages.auth_page import AuthPage
 import time
 from settings import *
 
-def test_authorisation(web_browser):
+def test_authorisation_with_valid_email(web_browser):
 
-    page = AuthPage(web_browser)
+    page = SmokeAuthPage(web_browser)
     page.enter_username(valid_email)
     page.enter_pass(valid_password)
     page.btn_click()
     time.sleep(10)
 
-    #assert page.get_current_url() == 'https://b2c.passport.rt.ru'
+    assert page.get_relative_link() == '/account_b2c/page'
 
 
 
+def test_authorisation_with_valid_phone(web_browser):
+
+    page = SmokeAuthPage(web_browser)
+    page.enter_username(valid_phone)
+    page.enter_pass(valid_password)
+    page.btn_click()
+    time.sleep(10)
+
+    assert page.get_relative_link() == '/account_b2c/page'
+
+# pytest --driver Chrome --driver-path \driver\chromedriver.exe tests\test_smoke.py
